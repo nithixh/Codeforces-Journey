16aca37d2180c52ddc3b4e9928e9fac2fd97cea3
Nithish Kumar T S <knithish938@gmail.com>
2026-07-10 19:16:12 +0530

    Update comments with problem details and idea
    Added problem statement and idea for Young Physicist.

---
+++ 800/69A-Young Physicist.py
@@ -0,0 +1,20 @@
+"""
+Problem: 69A - Young Physicist
+Rating: 800
+Link: http://codeforces.com/contest/69/problem/A
+
+Idea:
+If the sum of all x, y and z components is (0,0,0),
+the body is in equilibrium.
+"""
+n=int(input())
+x=y=z=0
+for i in range(n):
+    a,b,c=map(int,input().split())
+    x+=a
+    y+=b
+    z+=c
+if(x==y and y==z and x==0):
+    print("YES")
+else:
+    print("NO")
