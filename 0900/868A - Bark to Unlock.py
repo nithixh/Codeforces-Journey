68472aa7db030e9589c25bc2627f55750c6b54a8
Nithish Kumar T S <knithish938@gmail.com>
2026-08-21 07:20:36 +0530

    Update 868A - Bark to Unlock.py

--- 0900/868A - Bark to Unlock.py
+++ 0900/868A - Bark to Unlock.py
@@ -3,23 +3,25 @@ Problem: 868A - Bark to Unlock
 Rating: 900
 
 Idea:
-The password has only 2 characters. For it to appear as a substring
-after joining two words, the first word must end with the first character
-of the password and the second word must start with the second character.
+The password can either be one of the known words, or it can be formed by
+joining two words.
 
-Store the first and last characters of all words and check whether such
-a pair exists.
+For two words to form the password across their boundary, the first word
+must end with the first character of the password and the second word must
+start with the second character.
 """
 
 password=input()
 n=int(input())
 first=set()
 last=set()
+words=set()
 for _ in range(n):
     word=input()
+    words.add(word)
     first.add(word[0])
     last.add(word[1])
-if password[0] in last and password[1] in first:
+if password in words or (password[0] in last and password[1] in first):
     print("YES")
 else:
     print("NO")
