e65f84ad02103b308bbfe67647c8406e38977801
Nithish Kumar T S <knithish938@gmail.com>
2026-09-11 20:30:14 +0530

    Update 439A - Devu, the Singer and Churu, the Joker.py

--- 0900/439A - Devu, the Singer and Churu, the Joker.py
+++ 0900/439A - Devu, the Singer and Churu, the Joker.py
@@ -4,22 +4,21 @@ Rating: 900
 Link: https://codeforces.com/problemset/problem/439/A
 
 Idea:
-Devu needs sum(t) minutes to sing all n songs.
-
-He also needs 10 minutes of rest after every song except the last one,
-so the total required time is sum(t) + 10 * (n - 1).
-
-If this time is greater than d, it is impossible.
-
-Otherwise, the remaining time can be used by Churu for jokes.
+Devu needs sum(t) minutes to sing all the songs.
+He also needs 10 minutes of rest after every song except the last one.
+Therefore, the minimum time required to conduct the event is:
+sum(t) + 10 * (n - 1)
+If this is greater than d, it is impossible.
+The 10-minute rest periods can be used by Churu to tell jokes.
+So the total time available for Churu is simply d - sum(t).
 Each joke takes 5 minutes, so the maximum number of jokes is
-the remaining time divided by 5.
+(d - sum(t)) // 5.
 """
 
 n, d = map(int, input().split())
 t = list(map(int, input().split()))
-required = sum(t) + 10 * (n - 1)
-if required > d:
+song_time = sum(t)
+if song_time + 10 * (n - 1) > d:
     print(-1)
 else:
-    print((d - required) // 5)
+    print((d - song_time) // 5)
